b85d8b4f03b825b2aabee76410c6fadd17b77dfa
Amru <second.kautsar@gmail.com>
2023-09-30 15:43:59 +0700

    modify dashboard a little

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>
-      Dashboard belum dibikin, go to http://localhost:3000/login atau
-      http://localhost:3000/register
+    <div className="h-screen text-2xl flex flex-col font-bold w-full justify-center items-center">
+      <div>Dashboard belum dibikin bro, go to :</div>
+      <div className='text-blue-600'>http://localhost:3000/login atau http://localhost:3000/register</div>
     </div>
   );
 }
